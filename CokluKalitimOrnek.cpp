14ee4ad0f7ba922495565d6ee406cdf79ae1338f
Mustafa Emir ATA <148717139+mustafaemirata@users.noreply.github.com>
2024-12-02 23:01:09 +0300

    En basit haliye çoklu kalıtım yapısı

---
+++ CokluKalitimOrnek.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+using namespace std;
+
+class a
+{
+	int x;
+public:
+	int f1()
+	{
+		return x = 10;
+	}
+};
+
+class b
+{
+	int y;
+public:
+	int f2()
+	{
+		return y = 310;
+	}
+};
+
+class c : public a, public b
+{
+	int z;
+public:
+	int f3()
+	{
+		return z = 17;
+	}
+};
+
+int main()
+{
+	c nesne;
+	cout << "f1 sonucu: " << nesne.f1() << endl;
+	cout << "f2 sonucu: " << nesne.f2() << endl;
+	cout << "f3 sonucu: " << nesne.f3() << endl;
+
+}
