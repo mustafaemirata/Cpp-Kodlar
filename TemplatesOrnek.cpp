4b06b1c6f298503e21887cb19af1442ec803a868
Mustafa Emir ATA <148717139+mustafaemirata@users.noreply.github.com>
2024-12-12 14:29:44 +0300

    Template Kullanım Örneği

---
+++ TemplatesOrnek.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+template<typename T>
+T topla(T a, T b)
+{
+	return a + b;
+}
+// şablon fonksiyonu bir fonksiyon değildir.
+// Fonksiyon üretme planıdır. Farklı metotlar oluşturmak yerine bu yöntem kullanılır.
+int main()
+{
+	cout << topla(3, 4) << endl;
+	cout << topla(13.64, 98.23) << endl;
+	cout << topla(34.2f, 54.8f) << endl;
+}
