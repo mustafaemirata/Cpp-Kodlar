46ddb364469d05aa4644b0e0524ce28b1b4da3c3
Mustafa Emir ATA <148717139+mustafaemirata@users.noreply.github.com>
2024-12-21 15:34:02 +0300

    ObjectPointer

---
+++ ObjectPointerOrnek.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+
+class Employee
+{
+	int empId;
+	string empName;
+	public:
+	Employee(){}
+	Employee(int id, string isim)
+	{
+		empId = id;
+		empName = isim;
+	}
+	void display()
+	{
+		cout << "Isim: " << empName << endl;
+		cout << "Id No: " << empId << endl;
+	}
+};
+
+int main()
+{
+	Employee* obj = new Employee[2]{ Employee(101,"Mustafa"),Employee(202,"Eymen") };
+	
+	obj->display();
+	//obj elemanınınn bir sonraki nesneye geçebilmesi için bu kullanılır.
+	(++obj)->display();
+	delete [] obj;
+}
