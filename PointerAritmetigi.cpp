67bbdc4541b8bdaca81452a85f2d138a5a5173a5
Mustafa Emir ATA <148717139+mustafaemirata@users.noreply.github.com>
2024-12-21 16:06:20 +0300

    Pointer ile dizi tarama

---
+++ PointerAritmetigi.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+
+	int arr[7] = { 2,6,99,654,-1,84,0 };
+	cout << arr << endl;
+	int* ptr;
+	ptr = arr;
+	cout << ptr << endl;
+	//Dizinin ilk değerini yazdırır.
+	cout << *ptr << endl;
+	//Dizinin ikinci değerini yazdırır.
+	cout << *(ptr + 1) << endl;
+
+	//Her elemanı yazdırır.
+	for (int i = 0; i < 7; i++)
+	{
+		cout << "*ptr" << endl;
+		ptr++;
+	}
+
+}
+
