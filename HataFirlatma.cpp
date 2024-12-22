f5fcf7bb149627f8d071189a7ea0a36d3db01901
Mustafa Emir ATA <148717139+mustafaemirata@users.noreply.github.com>
2024-12-22 14:16:54 +0300

    Exceptions
    0'a bölme hatası için yazılmış exception örneği.

---
+++ HataFirlatma.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+class Sinif
+{
+public: 
+	int sayi1;
+	int sayi2;
+	int hesapla()
+	{
+		if (sayi2 == 0)
+		{
+			throw "Hatali islem yaptiniz: 0'a bolme islemi";
+		}
+		return sayi1 / sayi2;
+	}
+ };
+
+int main()
+{
+	try
+	{
+		Sinif nes;
+		nes.sayi1 = 10;
+		nes.sayi2 = 0;
+		cout << "Bolum: " << nes.hesapla() << endl;
+	}
+	catch(const char ex)
+	{
+		cout << ex << endl;
+	}
+}
