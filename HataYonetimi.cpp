268d262c7b714978fd6cf187bd9b2509b38c1f96
Mustafa Emir ATA <148717139+mustafaemirata@users.noreply.github.com>
2024-12-22 14:35:54 +0300

    Hata Yonetimi
    Birden fazla exception durumlarını inceleyerek ele alma.

---
+++ HataYonetimi.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+using namespace std;
+
+class Ornekler
+{
+public:
+	double bolme(int a, int b)
+	{
+		try
+		{
+			if (a < 0 || b < 0) throw 5;
+			if (b == 0) throw 1;
+			else
+			{
+				return a / b;
+			}
+		}
+		catch (int deger)
+		{
+			if (deger == 1)
+			{
+				cout << "0'a bolme hatasi." << endl;
+				
+			}
+			if (deger == 5)
+			{
+				cout << "Lutfen negatif sayilari bolmeyiniz." << endl;
+				
+			}
+		}
+	}
+};
+
+int main()
+{
+	Ornekler ornek;
+	ornek.bolme(-53, 0);
+}
