45ad8a35d3f3595e65a51769dd8499818c420e28
Mustafa Emir ATA <148717139+mustafaemirata@users.noreply.github.com>
2025-01-03 21:56:17 +0300

    Virtual Yapısı
    Bu örnekte polimorfizm ve kalıtım kullanılarak destructor çalışması yapıldı.

---
+++ virtaulYapisi.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+using namespace std;
+
+class HavaSavunmasi
+{
+private:
+	string isim;
+	int yapimYili;
+
+public:
+
+	HavaSavunmasi(string isim, int yil): isim(isim),yapimYili(yil){}
+	virtual void ses() = 0;
+
+	virtual void bilgiVer()
+	{
+		cout << "Sistemin ismi= " << isim << endl;
+		cout << "Sistemin yapim yili= " << yapimYili << endl;
+	}
+
+
+	virtual ~HavaSavunmasi() 
+	{
+		cout << "HavaSavunmasi yikici cagirildi." << endl;
+	}
+};
+
+class Ucak : public HavaSavunmasi
+{
+public:
+	Ucak(string isim, int yapimYili):HavaSavunmasi(isim, yapimYili){}
+	void bilgiVer() 
+	{
+		cout << "Ucak Bilgisi:" << endl;
+		HavaSavunmasi::bilgiVer(); 
+	}
+	~Ucak()
+	{
+		cout << "Ucak destructor cagirildi." << endl;
+	}
+
+	void ses() override
+	{
+		cout << "Ucak sesi: Vuuuu!" << endl;
+	}
+};
+
+int main()
+{
+	Ucak ucak1("KIZIL ELMA", 2011);
+	ucak1.ses();
+	ucak1.bilgiVer();
+
+}
