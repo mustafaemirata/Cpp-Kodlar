71afe0a9d3af5d28f0a1dfb5a83d412385dfc707
Mustafa Emir ATA <148717139+mustafaemirata@users.noreply.github.com>
2025-01-03 21:04:18 +0300

    Hata Yönetim Kontrolü
    Sıfıra bölme hatası exception ile incellendi

---
+++ BolmeHataYakalama.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int pay = 5;
+	int payda = 0;
+
+	try
+	{
+		if (payda == 0)
+		{
+			throw payda;
+		}
+		else
+		{
+			int sonuc = pay / payda;
+			cout << "Sonuc: " << sonuc << endl;
+		}
+	
+	}
+	catch (int hata)
+	{
+		cout << "Sifira bolme hatasi!" << endl;
+	}
+}
